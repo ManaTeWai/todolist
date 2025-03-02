9bc3023d79121a2798e00a20d4ba606e07aea319
Илья Манаков <89255913+IlyaManakov@users.noreply.github.com>
2025-03-02 20:41:22 +0300

    Delete backend directory
    Начинаем сначала backend (если что резерв копия есть на облаке)

--- backend/src/models/user.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from ..databases.database import Base
-
-# Модель пользователя
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
-    email = Column(String(100), unique=True)
-    password = Column(String(100))
